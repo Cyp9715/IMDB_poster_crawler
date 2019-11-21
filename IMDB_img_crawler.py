e7d64b90a814738666afff811094dbedbb38ed69
Cyp[Lee Byoung Gyu] <cypher9715@naver.com>
2019-11-21 09:57:40 +0900

    Typo correction & add user location setting.
    Typo correction & add user location setting.

--- IMDB_img_crawler.py
+++ IMDB_img_crawler.py
@@ -4,12 +4,21 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import urllib.parse
 
+# specify the location of file or variable, You don't have to do anything...
+cromdriver_location = './Basic_Data/chromedriver'
+tconstfile_location = './Basic_Data/Movie_Genres.tsv'
+savefile_location = './IMDB_img/'
+errorfile_location = './IMDB_img/error.tsv'
+
 T_const = []
 driver = webdriver
+counter_lock = Lock()
 
+
+# 이곳에서 할당을 안 하기에 T_const global 을 선언 해 주지 않아도 오류가 나지 않는다. 만약 할당을 해 준다면 오류가 발생할 것이다.
 class Base:
     def Tconst_List(self):
-        with open('./Basic_Data/Movie_Genres.tsv') as f:
+        with open(tconstfile_location) as f:
             for line in f:
                 T_const.append(line[:9])
             return T_const
@@ -33,14 +42,14 @@ def crawling(number):
             options = webdriver.ChromeOptions()
             options.add_argument('headless')
             options.add_argument('disable-gpu')
-            driver = webdriver.Chrome(executable_path=r'./Basic_Data/chromedriver', chrome_options=options)
+            driver = webdriver.Chrome(executable_path=cromdriver_location, chrome_options=options)
             driver.get("https://www.imdb.com/title/" + T_const[number])
             driver.implicitly_wait(2)
             driver.find_element_by_class_name("poster").click()
             url = driver.find_element_by_xpath(
                 "// *[ @ id = \"photo-container\"] / div / div[3] / div / div[2] / div[1] / div[2] / div / img[2]").get_attribute(
                 "src")
-            urllib.request.urlretrieve(url, "./IMDB_img/" + T_const[number] + ".jpg")
+            urllib.request.urlretrieve(url, savefile_location + T_const[number] + ".jpg")
             base.counter_plus()
             print(T_const[number] + " | " + str(counter.value) + "/" + str(T_const.__len__()) + " | " +
                   str(round(counter.value / T_const.__len__() * 100, 3)) + "%")
@@ -50,14 +59,27 @@ def crawling(number):
         except NoSuchElementException:
             link = urllib.parse.quote("www.imdb.com/title/" + T_const[number])
             print(("-" * 75) + "\n" + T_const[number] +
-                  " | NoSuchElementExcpetion, make sure "
+                  " | NoSuchElementException, make sure "
                   "Website have a poster image." +
                   "\n" + "https://" + link + "\n" + ("-" * 75))
+
+            write_error = errorfile_location
+            with open(write_error, 'a', encoding='utf-8') as e:
+                e.write((("-" * 75) + "\n" + T_const[number] +
+                         " | NoSuchElementException, make sure "
+                         "Website have a poster image." +
+                         "\n" + "https://" + link + "\n" + ("-" * 75)))
+                e.write("\n")
+
             base.counter_plus()
             break
 
         except:
-            print(T_const[number])
+            print("Retry!!" + T_const[number])
+            write_error = errorfile_location
+            with open(write_error, 'a', encoding='utf-8') as e:
+                e.write("Retry : " + T_const[number])
+                e.write("\n")
 
         finally:
             driver.quit()
